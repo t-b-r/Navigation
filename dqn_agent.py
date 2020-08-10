8146e44f536810ba8513dd0faa65214ea8abeaa7
Taylor R <truss88@gmail.com>
2020-08-10 07:21:56 -0400

    fix: add missing arugments to dqn_model.py
    Optimization was not previously running because number of nodes in the
    hidden layers was not properly specified.

--- dqn_agent.py
+++ dqn_agent.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from collections import namedtuple, deque
 
-from model import QNetwork
+from dqn_model import QNetwork
 
 import torch
 import torch.nn.functional as F
@@ -31,7 +31,7 @@ class Agent():
         """
         self.state_size = state_size
         self.action_size = action_size
-        self.seed = random.seed(seed)
+        self.seed = seed
 
         # Q-Network
         self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
@@ -42,6 +42,11 @@ class Agent():
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
+
+        #debugging
+        print(state_size)
+        print(action_size)
+        print(seed)
 
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
@@ -63,6 +68,8 @@ class Agent():
             state (array_like): current state
             eps (float): epsilon, for epsilon-greedy action selection
         """
+
+
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
         self.qnetwork_local.eval()
         with torch.no_grad():
@@ -74,6 +81,7 @@ class Agent():
             return np.argmax(action_values.cpu().data.numpy())
         else:
             return random.choice(np.arange(self.action_size))
+
 
     def learn(self, experiences, gamma):
         """Update value parameters using given batch of experience tuples.
@@ -93,7 +101,7 @@ class Agent():
         Q_targets = rewards + (gamma*Q_targets_next*(1-dones))
 
         # Get expected Q values from local model
-        Q_expected = self.qnetwork_local(states).gather(1.actions)
+        Q_expected = self.qnetwork_local(states).gather(1, actions)
         # Compute loss
         loss = F.mse_loss(Q_expected, Q_targets)
 
