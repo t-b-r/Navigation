6bf4524e7f22b351d8cef94f036c73045ff86c05
Taylor R <truss88@gmail.com>
2020-08-11 08:04:02 -0400

    remove debugging prints

--- dqn_agent.py
+++ dqn_agent.py
@@ -43,11 +43,6 @@ class Agent():
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
 
-        #debugging
-        print(state_size)
-        print(action_size)
-        print(seed)
-
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
         self.memory.add(state, action, reward, next_state, done)
@@ -107,9 +102,6 @@ class Agent():
         # Minimize the loss
         self.optimizer.zero_grad()
         loss.backward()
-        self.optimizer.step()
-
-
         # ------------------- update target network ------------------- #
         self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
 
