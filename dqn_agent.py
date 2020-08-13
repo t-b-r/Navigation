6891d4e38194332073ee37990e616f79c047c1c9
Taylor R <truss88@gmail.com>
2020-08-13 07:17:19 -0400

    fix: paramters for Q_targets_next

--- dqn_agent.py
+++ dqn_agent.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
+BATCH_SIZE = 32         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate
@@ -63,8 +63,6 @@ class Agent():
             state (array_like): current state
             eps (float): epsilon, for epsilon-greedy action selection
         """
-
-
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
         self.qnetwork_local.eval()
         with torch.no_grad():
@@ -88,9 +86,8 @@ class Agent():
         """
         states, actions, rewards, next_states, dones = experiences
 
-
         # get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[1].unsqueeze(1)
         # compute Q targets for current states
         Q_targets = rewards + (gamma*Q_targets_next*(1-dones))
 
